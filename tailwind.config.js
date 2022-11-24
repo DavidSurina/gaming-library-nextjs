4e268feac2aaec10cf34a3748628a1f7fd2df841
DavePersonal <david.surina22@gmail.com>
2022-11-24 23:40:49 +0100

    added new colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
     './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    fontFamily: {
+        sans: ['Roboto', 'sans-serif'],
+    },
+    colors: {
+      backgroundGrey: '#222629',
+      contrastText: '#fcfeff',
+      dimmedGrey: '#6b6e70',
+      darkGrey: '#474b4f',
+      darkGreen: '#61892f',
+      lighterGreen: '#86c232',
+    },
     extend: {},
   },
   plugins: [],
