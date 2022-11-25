21502bc5e8584e69d6fb1e9d3c770999e4beab4f
DavePersonal <david.surina22@gmail.com>
2022-11-25 23:47:31 +0100

    added globabl button style

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -8,10 +8,7 @@ function LoginPage(): JSX.Element {
       <input type="email" aria-label="email" />
       <label>Password</label>
       <input type="password" aria-label="password" />
-      <button
-        className="px-4 py-2 mt-3 shadow-sm text-contrastText self-center bg-lighterGreen"
-        type="submit"
-      >
+      <button className="front-page_btn" type="submit">
         Login
       </button>
     </form>
